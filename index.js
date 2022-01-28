722184bebafc6028b10c3d5dd680e4cf52d4a5c7
Eddy Gaspar <eddyg@Eddys-MBP.lan>
2022-01-27 19:25:51 -0800

    JS Graffiti activity

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Select some elements...
+// Change header element
+let header = document.querySelector('#page-header')
+
+header.style.textAlign = 'left'
+
+// Select multiple elements
+let images = document.querySelectorAll('.dog-image')
+
+for (let i = 0; i < images.length; i++) {
+    images[i].style.borderRadius = '50px'
+    images[i].style.borderStyle = 'solid'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+
+for (let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign = 'left'
+    dogNames[i].style.color = 'green'
+}
+
+let footer = document.querySelector('.footer')
+
+footer.style.color = 'orange'
